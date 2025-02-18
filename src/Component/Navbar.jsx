ca569564b0b4a74e7df77c38158134143ed30b86
vinoth12345678910 <anandgani85@gmail.com>
2025-02-18 17:25:01 +0530

    Updated some features

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
-import NavLinks from './NavLinks';
+import Navlinks from './Navlinks';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from '../features/user/userSlice';
@@ -20,12 +20,12 @@ const Navbar = () => {
       <div className='navbar align-element'>
         <div className='navbar-start'>
           {/* TITLE */}
-          <NavLink
+          <Navlink
             to='/'
             className='hidden lg:flex btn btn-primary text-3xl items-center'
           >
             C
-          </NavLink>
+          </Navlink>
           {/* DROPDOWN */}
           <div className='dropdown'>
             <label tabIndex={0} className='btn btn-ghost lg:hidden'>
@@ -35,13 +35,13 @@ const Navbar = () => {
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'
             >
-              <NavLinks />
+              <Navlinks />
             </ul>
           </div>
         </div>
         <div className='navbar-center hidden lg:flex'>
           <ul className='menu menu-horizontal'>
-            <NavLinks />
+            <Navlinks />
           </ul>
         </div>
         <div className='navbar-end'>
@@ -54,14 +54,14 @@ const Navbar = () => {
             <BsMoonFill className='swap-off h-4 w-4' />
           </label>
           {/* CART LINK */}
-          <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
+          <Navlink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
               <BsCart3 className='h-6 w-6' />
               <span className='badge badge-sm badge-primary indicator-item'>
                 {numItemsInCart}
               </span>
             </div>
-          </NavLink>
+          </Navlink>
         </div>
       </div>
     </nav>
